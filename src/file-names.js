706955cb34909ce658cf24c829e6c69106dd2cbb
Igor Bochenkov <igor.bochenkoff@gmail.com>
2025-01-06 21:57:14 +0300

    feat: implement task 'File names'

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const resultArr = [];
+  let count = 0;
+
+  names.forEach((item) => {
+    if (!resultArr.includes(item)) {
+      resultArr.push(item);
+      count = 0;
+    } else {
+      count++;
+      resultArr.push(`${item}(${count})`);
+    }
+  });
+
+  return resultArr;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
