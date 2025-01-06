5a840d632d883ba155b27bf2facfbe65e095c0e0
Igor Bochenkov <igor.bochenkoff@gmail.com>
2025-01-06 22:39:18 +0300

    feat: implement task 'Encode line'

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let resultStr = "";
+  let count = 1;
+
+  let arr = str.split("");
+
+  arr.forEach((item, index, array) => {
+    if (item == array[index + 1]) {
+      count++;
+    } else {
+      if (count == 1) {
+        resultStr += item;
+      } else {
+        resultStr += `${count}${item}`;
+        count = 1;
+      }
+    }
+  });
+  return resultStr;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
